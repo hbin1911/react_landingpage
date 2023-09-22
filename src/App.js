cfa1fda9935945f85d084628337ec927785ccac3
hbin1911 <rajas.mulik@gmail.com>
2023-09-22 17:37:53 +0530

    added home component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import "./styles/App.scss"
 import "./styles/Header.scss"
+import "./styles/home.scss"
+import Home from "./components/Home";
 
 function App() {
   return (
@@ -9,7 +11,7 @@ function App() {
 
       <Header />
       <Routes>
-        <Route />
+        <Route path="/" element={<Home/>}/>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return <div className='home'>
+      <main>
+        <h1>Techystar</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem, ipsam.</p>
+      </main>
+    </div>
+  
+}
+
+export default Home
